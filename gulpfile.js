0136eea6fba8abea17ec6d79be098b4af4cac4f2
JJ Ying <yingjunjiu@gmail.com>
2019-08-26 10:42:47 +0800

    更新 CDN 地址

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,20 @@ var gulp = require('gulp'),
     plugins = gulpLoadPlugins();
 
 var cdnUrl = [
-	[ '../assets/', 'https://anyway-web.b0.upaiyun.com/anywayfm.com/' ]
+	[ '../assets/', 'https://s.anw.red/anywayfm.com/' ]
 ];
- 
+
 gulp.task('watch', function() {
 	gulp.watch(['*','*/*'], ['default']);
  });
- 
- 
+
+
 gulp.task('default', function() {
 	gulp.src(['assets/*.css','!assets/*.min.css'])
 		.pipe(plugins.cleanCss({compatibility: 'ie8'}))
 		.pipe(plugins.batchReplace(cdnUrl))
 		.pipe(plugins.rename({
-		      suffix: '.min'
-		    }))
-		.pipe(gulp.dest('builds/'));     
+	      suffix: '.min'
+	    }))
+		.pipe(gulp.dest('builds/'));
 });
-
